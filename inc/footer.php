f9a2b408798abbf3f475de206c9b44df1e17a6e6
CoDemented <52868823+CoDemented@users.noreply.github.com>
2020-02-09 22:56:29 +0500

    sdf

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,29 @@
+
+<footer class="footer">
+    <div class="wrapper">
+        <div class="widget-area">
+            <div class="widg">1</div>
+            <div class="widg">2</div>
+            <div class="widg">3</div>
+            <div class="widg">4</div>
+        </div>
+    </div>
+    <div class="copyright-bar">
+        <div class="wrapper">
+            <p class="copyright">Copyright - 2020. Developed by <a href="#"> CoDementeD </a> All-Rights Reserved.</p>
+        </div>
+    </div>
+</footer>
+<style>
+    .copyright-bar {
+        background: #bfbfbf;
+        padding: 5px;
+    }
+</style>
+
+
+
+
 <script src="<?php echo _ASSETS; ?>js/jquery341.js"></script>
 <script src="<?php echo _ASSETS; ?>js/index.js"></script>
 <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
